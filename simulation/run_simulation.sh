ef29b5e83612284b942d04e50074a70d12da4795
r1ckya <geraskin.ia@yandex.ru>
2021-05-23 20:18:42 +0300

    add simulation run script

---
+++ simulation/run_simulation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Start simulation run!"
+
+mkdir -p results
+
+echo "Generation data..."
+# default p_i is 1/20 = 0.05
+python simulate.py \
+    --p_i_range 0.04 0.07 \
+    --p_ji_range 0.4 0.6 \
+    --n_seq 5000 \
+    --n_pairs 3 \
+    --n_randoms 2 \
+    > data.txt \
+    2> /dev/null
+echo "Data generated!"
+
+echo "Processing alignment..."
+python ../process_aln.py \
+    --out_dir results \
+    --p_ij_cutoff 0 \
+    --p_j_cond_i_cutoff 0 \
+    < data.txt \
+    > results.csv \
+    2> /dev/null
+echo "Processing done!"
+
+echo "Run done!"

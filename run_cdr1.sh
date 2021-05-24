1db269f427341542783bcb23a0e2c718d9afd808
r1ckya <geraskin.ia@yandex.ru>
2021-05-24 17:30:02 +0300

    rename result file

--- run_cdr1.sh
+++ run_cdr1.sh
@@ -4,7 +4,7 @@ echo "Running pipeline for cdr1 regions..."
 ./run.sh \
     -f data/cdr1_filter_seq.txt \
     -i data/cdr1_aa_len500.txt \
-    -o data/ratios.csv \
+    -o data/results.csv \
     -d results \
     -a data/cdr1_clustalo.txt
 echo "cdr1 pipeline done!"
